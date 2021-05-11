be79493c26a68aa6a1dd645fe59f661a97a3196b
Olatunji Abayomi <abayomijohn273@gmail.com>
2021-05-11 02:11:06 +0100

    Custom errorResponse class

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
+const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db')
 // Routes files
 const bootcamps = require('./routes/bootcamps');
@@ -20,6 +21,9 @@ app.use(express.json());
 
 // Mount routes
 app.use('/api/v1/bootcamps', bootcamps);
+
+// since error handler is a middleware, it is declared in the entry file server.js and  placed after the routers is declares
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
